ef1cd6caba917c819ddc075e3623bf11da9fe135
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2024-07-25 13:47:07 +0100

    chore: Refactor theme provider imports and remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,39 @@
 import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto"></section>
-      <div className="sub-container maw-w-[496px]">
-        <Image
-          src="/assets/icons/logo-full.svg"
-          alt="logo image"
-          width={1000}
-          height={1000}
-          className="mb-12 h-10 w-fit"
-        />
-        <PatientForm />
-        <div className="text-14-regular">
-        © 2024 CarePluse 
+      <section className="remove-scrollbar container my-auto">
+        {/* OTP verification*/}
+        <div className="sub-container maw-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo image"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className=" text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
-      </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient image"
+        height={1000}
+        width={1000}
+        className="maw-w-[50%] side-img"
+      />
     </div>
   );
 }
---
+++ components/CustomFormField.tsx
@@ -0,0 +1,97 @@
+"use client";
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+import Image from "next/image";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import { E164Number } from "libphonenumber-js/core";
+
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  label?: string;
+  name: string;
+  placeholder?: string;
+  description?: string;
+  required?: boolean;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+}
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  switch (props.fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {props.iconSrc && (
+            <Image
+              src={props.iconSrc}
+              alt={props.iconAlt || "icon"}
+              width={20}
+              height={20}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={props.placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="TN"
+            international
+            placeholder={props.placeholder}
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </FormControl>
+      );
+    default:
+      break;
+  }
+};
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, label, name } = props;
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+          <RenderField field={field} props={props} />
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
